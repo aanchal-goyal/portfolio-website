4e748947b088e81cba31e3c4c07a46ef48064b78
Aanchal Goyal <aanchalgoyal.dev@gmail.com>
2026-01-31 19:15:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const toggle = document.getElementById("theme-toggle");
+
+toggle.addEventListener("click", () => {
+    document.body.classList.toggle("light");
+
+    if (document.body.classList.contains("light")) {
+        toggle.textContent = "🌞";
+    } else {
+        toggle.textContent = "🌙";
+    }
+});
